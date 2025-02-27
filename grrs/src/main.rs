262557f4e3d88f44d15b514b031ddec3b6e02638
wangzhizhou <824219521@qq.com>
2025-02-28 02:28:01 +0800

    refine read file and panic

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -5,12 +5,9 @@ struct Cli {
     pattern: String,
     path: std::path::PathBuf, // file system paths work for cross-platform
 }
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let content = std::fs::read_to_string(&args.path)?;
+    println!("file content: {}", content);
+    Ok(())
 }
